ecf84fe3596d61ec29a0e64190d13ef5b4996c25
Mai Thanh Danh <mtd.maithanhdanh@gmail.com>
2020-11-12 00:56:08 +0700

    [UPDATE] unit test for customer feature

---
+++ tests/controllers/customer.controller.test.js
@@ -0,0 +1,190 @@
+const { Customer } = require("../../models/customer.model")
+const server = require("../../index")
+const request = require("supertest")
+const mongoose = require("mongoose")
+const uid = 1
+const queryString = "name=asdf&price=0%2C100000&color=red"
+const productId = "42109"
+
+beforeAll(async () => {
+	jest.useFakeTimers()
+	var newCustomer = new Customer({ uid })
+	await newCustomer.save()
+})
+
+afterAll(async () => {
+	await Customer.deleteMany({})
+	server.close()
+})
+
+describe("Customer", () => {
+	describe("global app status", () => {
+		it("should return ok", async () => {
+			const res = await request(server).get("/status").expect(200)
+			expect(res.body).toBe("OK")
+		})
+	})
+
+	describe("user routes status", () => {
+		it("should return ok", async () => {
+			const res = await request(server).get("/customer/status").expect(200)
+			expect(res.body).toBe("OK")
+		})
+	})
+
+	describe("/ => get user", () => {
+		it("success", async () => {
+			const res = await request(server)
+				.get("/customer/")
+				.set("Cookie", `uid=${uid}`)
+				.expect(200)
+
+			expect(res.body.error).toBe(false)
+			expect(Object.keys(res.body.response).length).toBe(2)
+			expect(res.body.response.uid).toBe(uid.toString())
+		})
+
+		it("failed => missing uid", async () => {
+            const res = await request(server).get("/customer/").expect(400)
+            expect(res.body.error).toBe(true)
+        })
+        it("failed => invalid uid", async () => {
+            var uid = 2
+			const res = await request(server)
+				.get(`/customer/`)
+				.set("Cookie", `uid=${uid}`)
+				.expect(400)
+
+			expect(res.body.error).toBe(true)
+			expect(res.body.response).toBe('invalid uid')
+		})
+		it("failed => invalid uid", async () => {
+			const res = await request(server)
+				.get("/customer/")
+				.set("Cookie", `uid=`)
+                .expect(400)
+                expect(res.body.error).toBe(true)
+		})
+	})
+
+	describe("/add_search => add search event", () => {
+		it("success", async () => {
+			const res = await request(server)
+				.get(`/customer/add_search`)
+				.set("Cookie", `uid=${uid}`)
+				.send({ queryString: queryString })
+				.expect(200)
+			expect(res.body.error).toBe(false)
+			expect(Object.keys(res.body.response).length).toBe(2)
+			expect(res.body.response.uid).toBe(uid.toString())
+			expect(Object.keys(res.body.response.activity).length).toBe(2)
+			expect(res.body.response.activity.search.length).toBe(1)
+			expect(res.body.response.activity.search[0].query).toBe(queryString)
+        })
+        
+		it("success => add another search event", async () => {
+			const res = await request(server)
+				.get(`/customer/add_search`)
+				.set("Cookie", `uid=${uid}`)
+				.send({ queryString: queryString })
+				.expect(200)
+			expect(res.body.error).toBe(false)
+			expect(Object.keys(res.body.response).length).toBe(2)
+			expect(res.body.response.uid).toBe(uid.toString())
+			expect(Object.keys(res.body.response.activity).length).toBe(2)
+			expect(res.body.response.activity.search.length).toBe(2)
+			expect(res.body.response.activity.search[1].query).toBe(queryString)
+		})
+
+		it("failed => missing uid", async () => {
+			const res = await request(server)
+				.get(`/customer/add_search`)
+				.set("Cookie", `uid=`)
+				.send({ queryString: queryString })
+				.expect(400)
+
+			expect(res.body.error).toBe(true)
+        })
+        it("failed => invalid uid", async () => {
+            var uid = 2
+			const res = await request(server)
+				.get(`/customer/add_search`)
+				.set("Cookie", `uid=${uid}`)
+				.send({ queryString: queryString })
+				.expect(400)
+
+			expect(res.body.error).toBe(true)
+			expect(res.body.response).toBe('invalid uid')
+		})
+		it("failed => missing queryString", async () => {
+			const res = await request(server)
+				.get(`/customer/add_search`)
+				.set("Cookie", `uid=${uid}`)
+				.expect(400)
+			expect(res.body.error).toBe(true)
+		})
+    })
+    
+	describe("/add_view => add view event", () => {
+        beforeAll(async () => {
+            await Customer.deleteMany({})
+            var newCustomer = new Customer({ uid })
+	        await newCustomer.save()
+        })
+		it("success", async () => {
+			const res = await request(server)
+				.get(`/customer/add_view`)
+				.set("Cookie", `uid=${uid}`)
+				.send({ productId: productId })
+                .expect(200)
+			expect(res.body.error).toBe(false)
+			expect(Object.keys(res.body.response).length).toBe(2)
+			expect(res.body.response.uid).toBe(uid.toString())
+			expect(Object.keys(res.body.response.activity).length).toBe(2)
+			expect(res.body.response.activity.view.length).toBe(1)
+			expect(res.body.response.activity.view[0].productId).toBe(productId)
+        })
+        
+		it("success => add another view event", async () => {
+			const res = await request(server)
+				.get(`/customer/add_view`)
+				.set("Cookie", `uid=${uid}`)
+				.send({ productId: productId })
+				.expect(200)
+			expect(res.body.error).toBe(false)
+			expect(Object.keys(res.body.response).length).toBe(2)
+			expect(res.body.response.uid).toBe(uid.toString())
+			expect(Object.keys(res.body.response.activity).length).toBe(2)
+			expect(res.body.response.activity.view.length).toBe(2)
+			expect(res.body.response.activity.view[1].productId).toBe(productId)
+		})
+
+		it("failed => missing uid", async () => {
+			const res = await request(server)
+				.get(`/customer/add_view`)
+				.set("Cookie", `uid=`)
+				.send({ productId: productId })
+				.expect(400)
+
+			expect(res.body.error).toBe(true)
+		})
+		it("failed => invalid uid", async () => {
+            var uid = 2
+			const res = await request(server)
+				.get(`/customer/add_view`)
+				.set("Cookie", `uid=${uid}`)
+				.send({ productId: productId })
+				.expect(400)
+
+			expect(res.body.error).toBe(true)
+			expect(res.body.response).toBe('invalid uid')
+		})
+		it("failed => missing queryString", async () => {
+			const res = await request(server)
+				.get(`/customer/add_view`)
+				.set("Cookie", `uid=${uid}`)
+				.expect(400)
+			expect(res.body.error).toBe(true)
+		})
+	})
+})
---
+++ tests/models/customer.mode.test.js
@@ -0,0 +1,153 @@
+const { Customer, ActivitySchema } = require("../../models/customer.model")
+const mongoose = require("../../config/mongoose")
+
+const uid = 1
+
+beforeAll(async () => {
+	await mongoose.connect()
+})
+
+afterAll(async () => {
+	await Customer.deleteMany({})
+})
+
+describe("activity schema", () => {
+	it("activity schema with empty data", () => {
+		var activitySchema = new ActivitySchema()
+
+		var searchItem = activitySchema.searchItem
+		expect(Object.keys(searchItem).length).toBe(2)
+		expect(searchItem).toHaveProperty("query")
+		expect(searchItem).toHaveProperty("date")
+
+		var viewItem = activitySchema.viewItem
+		expect(Object.keys(viewItem).length).toBe(2)
+		expect(viewItem).toHaveProperty("productId")
+		expect(viewItem).toHaveProperty("date")
+	})
+
+	it("activity schema with data", () => {
+		var query = "name=asdf&price=0%2C100000&color=red"
+		var activitySchema = new ActivitySchema(query)
+
+		var searchItem = activitySchema.searchItem
+		expect(Object.keys(searchItem).length).toBe(2)
+		expect(searchItem).toHaveProperty("query")
+		expect(searchItem.query).toBe(query)
+		expect(searchItem).toHaveProperty("date")
+
+		var view = "42109"
+		var activitySchema = new ActivitySchema(view)
+		var viewItem = activitySchema.viewItem
+		expect(Object.keys(viewItem).length).toBe(2)
+		expect(viewItem).toHaveProperty("productId")
+		expect(viewItem.productId).toBe(view)
+		expect(viewItem).toHaveProperty("date")
+	})
+})
+
+describe("customer schema and transformed format", () => {
+	beforeAll(async function () {
+		var newCustomer = new Customer({ uid })
+		return (doc = await newCustomer.save())
+	})
+
+	it("customer schema", () => {
+		expect(doc).toHaveProperty("uid")
+		expect(doc.uid).toBe("1")
+		expect(doc).toHaveProperty("activity")
+	})
+
+	it("transform method", () => {
+		var transformedDoc = doc.transform()
+
+		expect(Object.keys(transformedDoc).length).toBe(2)
+		expect(transformedDoc).toHaveProperty("uid")
+		expect(transformedDoc.uid).toBe("1")
+		expect(transformedDoc).toHaveProperty("activity")
+	})
+})
+
+describe("customer query", () => {
+	it("Check to create new customer => create failed", async () => {
+		var doc = await Customer.checkToCreateCustomer(uid)
+		expect(doc).toBe(null)
+	})
+	it("Check to create new customer => create success", async () => {
+		var uid = 100
+		var doc = await Customer.checkToCreateCustomer(uid)
+		expect(typeof doc).toBe("object")
+
+		var transformedDoc = doc.transform()
+		expect(Object.keys(transformedDoc).length).toBe(2)
+		expect(transformedDoc).toHaveProperty("uid")
+		expect(transformedDoc.uid).toBe(uid.toString())
+		expect(transformedDoc).toHaveProperty("activity")
+	})
+
+	it("get customer by uid", async () => {
+		var doc = await Customer.getCustomerUid(uid)
+		expect(typeof doc).toBe("object")
+
+		var transformedDoc = doc.transform()
+		expect(Object.keys(transformedDoc).length).toBe(2)
+		expect(transformedDoc).toHaveProperty("uid")
+		expect(transformedDoc.uid).toBe("1")
+		expect(transformedDoc).toHaveProperty("activity")
+	})
+
+	it("get customer by uid => invalid uid ", async () => {
+		var uid = 2
+		var doc = await Customer.getCustomerUid(uid)
+		expect(doc).toBe(null)
+	})
+
+	it("get customer by uid and update search event", async () => {
+		var searchItem = "name=asdf&price=0%2C100000&color=red"
+		var doc = await Customer.getByUidAndUpdate(uid, { searchItem })
+
+		expect(doc.activity.view.length).toBe(0)
+		var search = doc.activity.search[0]
+
+		expect(search.query).toBe(searchItem)
+		expect(search).toHaveProperty("date")
+	})
+
+	it("get customer by uid and update view event", async () => {
+		var viewItem = "42109"
+		var doc = await Customer.getByUidAndUpdate(uid, { viewItem })
+
+		expect(Object.keys(doc.activity.search).length).toBe(1)
+		var view = doc.activity.view[0]
+
+		expect(view.productId).toBe(viewItem)
+		expect(view).toHaveProperty("date")
+	})
+
+	it("get customer by uid and update => invalid uid", async () => {
+		var uid = 2
+		var viewItem = "42109"
+		var doc = await Customer.getByUidAndUpdate(uid, { viewItem })
+		expect(doc).toBe(null)
+	})
+})
+
+describe("Add multiple events", () => {
+	var viewItem = "42109"
+	var searchItem = "name=asdf&price=0%2C100000&color=red"
+	
+	beforeAll(async () => {
+		await Customer.getByUidAndUpdate(uid, { viewItem })
+		await Customer.getByUidAndUpdate(uid, { viewItem })
+	})
+
+	it("update multi events", async () => {
+		var doc = await Customer.getByUidAndUpdate(uid, { searchItem })
+
+		expect(Object.keys(doc.activity.view).length).toBe(3)
+		var view = doc.activity.view[0]
+
+		expect(view.productId).toBe(viewItem)
+		expect(view).toHaveProperty("date")
+	})
+})
